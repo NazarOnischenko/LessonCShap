663a039898bbd38aab96d31f6fd22558a73ff0e0
naz9559 <naz9559@gmail.com>
2020-11-18 12:04:25 +0200

    Updating this program

--- LessonCShap/Program.cs
+++ LessonCShap/Program.cs
@@ -6,7 +6,9 @@ namespace LessonCShap
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wrute your name:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello {0}!",name);
         }
     }
 }
